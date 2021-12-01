a6154fa56602225a71bce7748509beb6b76b6125
avinanick <avinanick@gmail.com>
2021-11-30 17:25:18 -0800

    Created initial stub functions to the state script.

--- components/State.gd
+++ components/State.gd
@@ -15,3 +15,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func physics_process(delta: float) -> void:
+	return
+
+func process(delta: float) -> void:
+	return
+
+func unhandled_input(event: float) -> void:
+	return
