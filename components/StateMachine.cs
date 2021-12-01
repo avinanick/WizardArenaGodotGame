75b74b820cb11e36645c251af323d4702ef04d02
avinanick <avinanick@gmail.com>
2021-11-30 18:47:12 -0800

    Set the basic processing methods in the state machine to call the corresponding methods in the current state.

--- components/StateMachine.cs
+++ components/StateMachine.cs
@@ -7,11 +7,29 @@ public class StateMachine : Node
     private NodePath InitialState;
     private State CurrentState = null;
 
+    public override void _PhysicsProcess(float delta)
+    {
+        base._PhysicsProcess(delta);
+        CurrentState.PhysicsProcess(delta);
+    }
+
+    public override void _Process(float delta)
+    {
+        base._Process(delta);
+        CurrentState.Process(delta);
+    }
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         base._Ready();
         CurrentState = GetNode<State>(InitialState);
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        base._UnhandledInput(@event);
+        CurrentState.UnhandledInput(@event);
     }
 
     public void SetState(State newState) {
