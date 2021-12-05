6f3386ee8fa0d67fd6d501c4b465c8a59aced6c9
avinanick <avinanick@gmail.com>
2021-12-04 17:27:14 -0800

    Started calculations in the player move state physics process, and created a stub method for calculate velocity.

--- components/PlayerMoveState.cs
+++ components/PlayerMoveState.cs
@@ -6,6 +6,7 @@ public class PlayerMoveState : PlayerState
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    private Vector3 Velocity = Vector3.Zero;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -13,13 +14,23 @@ public class PlayerMoveState : PlayerState
         base._Ready();
     }
 
+    private Vector3 CalculateVelocity() {
+        return Vector3.Zero;
+    }
+
     private static Vector3 GetInputDirection() {
-        return new Vector3();
+        return Vector3.Zero;
     }
 
     public override void PhysicsProcess(float delta)
     {
         base.PhysicsProcess(delta);
         Vector3 inputDirection = GetInputDirection();
+        Vector3 moveDirection = inputDirection;
+        if(moveDirection.Length() > 1) {
+            moveDirection = moveDirection.Normalized();
+        }
+        moveDirection.y = 0;
+        Velocity = CalculateVelocity();
     }
 }
