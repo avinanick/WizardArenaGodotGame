8ecfb2d9cd1aa2f078c81e93ca78f37f1d68b7ca
avinanick <avinanick@gmail.com>
2021-12-09 20:18:35 -0800

    Added and set a reference to the animation tree in the player model script.

--- game_objects/characters/PlayerModel.cs
+++ game_objects/characters/PlayerModel.cs
@@ -6,10 +6,18 @@ public class PlayerModel : Spatial
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    private enum States {
+        Idle,
+        Run,
+        Air
+    }
+    private AnimationTree Animator;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         base._Ready();
+        Animator = GetNode<AnimationTree>("AnimationTree");
+        Animator.Active = true;
     }
 }
