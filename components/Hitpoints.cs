c79682220f8f7cc484500e69a3a628f1fe8dc9f1
avinanick <avinanick@gmail.com>
2022-07-12 21:13:12 -0700

    Created some of the basic parts of the hitpoints component.

--- components/Hitpoints.cs
+++ components/Hitpoints.cs
@@ -6,11 +6,14 @@ public class Hitpoints : Node
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+    [Export]
+    private int MaxHealth = 10;
+    private int CurrentHealth = 0;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        CurrentHealth = MaxHealth;
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,4 +21,8 @@ public class Hitpoints : Node
 //  {
 //      
 //  }
+
+    public void TakeDamage(int amount) {
+        
+    }
 }
