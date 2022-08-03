e480d5e71dd2b135eb7df301b5eee452c8b982ba
avinanick <avinanick@gmail.com>
2022-08-02 21:12:18 -0700

    Created stubs for the basic functions in the component system.

--- components/ComponentSystem.cs
+++ components/ComponentSystem.cs
@@ -1,11 +1,10 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class ComponentSystem : Node
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    private Dictionary<int, Component> Components = new Dictionary<int, Component>();
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -13,9 +12,7 @@ public class ComponentSystem : Node
         
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public void RegisterComponent(int entityID) {
+
+    }
 }
