9bda4d2bced746d65f7fb21d2c5b1c3e30816242
avinanick <avinanick@gmail.com>
2022-08-02 21:07:15 -0700

    Created the stub functions that the basic component class will need.

--- components/Component.cs
+++ components/Component.cs
@@ -3,9 +3,7 @@ using System;
 
 public class Component : Node
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    private int EntityID;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -13,9 +11,11 @@ public class Component : Node
         
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    private void RegisterToSystem() {
+
+    }
+
+    private void SetEntityID() {
+
+    }
 }
