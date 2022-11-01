328f040a8f630577b6652ab3c8923397b86a85f6
avinanick <avinanick@gmail.com>
2022-10-31 20:19:20 -0700

    Implimented the base version of the register method for the component.

--- components/Component.cs
+++ components/Component.cs
@@ -4,6 +4,7 @@ using System;
 public class Component : Node
 {
     private int EntityID;
+    private ComponentSystem ComponentControl;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -11,8 +12,17 @@ public class Component : Node
         
     }
 
-    private void RegisterToSystem() {
+    private virtual string ComponentString() {
+        return "Component: component, Entity: " + EntityID;
+    }
 
+    private void RegisterToSystem() {
+        if(ComponentControl) {
+            ComponentControl.RegisterComponent(EntityID, this);
+        } else {
+            // Print some sort of error
+            GD.Print("Error: No component system found for " + this.ComponentString());
+        }
     }
 
     private void SetEntityID() {
